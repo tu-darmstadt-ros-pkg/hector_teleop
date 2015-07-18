93a5a23987d23606ee086197e1f380fcf20011a0
Christian Rose <christian.j.rose@gmx.de>
2015-07-18 12:06:56 +0200

    Added flipper control for bertl

--- hector_joy_teleop/src/joy_teleop.cpp
+++ hector_joy_teleop/src/joy_teleop.cpp
@@ -30,53 +30,88 @@
 #include <sensor_msgs/Joy.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/QuaternionStamped.h>
+#include <std_msgs/Float64.h>
 
 namespace JoyTeleop {
 
-  ros::Subscriber joyInput;
-  ros::Publisher motionCommandOutput;
-  ros::Publisher cameraCommandOutput;
+ros::Subscriber joyInput;
+ros::Publisher motionCommandOutput;
+ros::Publisher cameraCommandOutput;
 
-  geometry_msgs::Twist motionCommand;
-  geometry_msgs::QuaternionStamped cameraCommand;
+geometry_msgs::Twist motionCommand;
+geometry_msgs::QuaternionStamped cameraCommand;
 
-  int axis_speed;
-  double speedForward;
-  double speedBackward;
-  int axis_steer;
-  double steerAngle;
-  int button_crab;
-  int button_slow;
-  int button_brake;
-  double slowFactor;
+int axis_speed;
+double speedForward;
+double speedBackward;
+int axis_steer;
+double steerAngle;
+int button_crab;
+int button_slow;
+int button_brake;
+double slowFactor;
 
-  int axis_cameraPan;
-  int axis_cameraTilt;
-  int button_cameraReset;
-  std::string cameraMode;
-  double cameraSpeed;
-  double cameraMaxPan;
-  double cameraMaxTiltDown;
-  double cameraMaxTiltUp;
+int axis_cameraPan;
+int axis_cameraTilt;
+int button_cameraReset;
+std::string cameraMode;
+double cameraSpeed;
+double cameraMaxPan;
+double cameraMaxTiltDown;
+double cameraMaxTiltUp;
 
-  double cameraPan = 0;
-  double cameraTilt = 0;
-  double cameraPanSpeed = 0;
-  double cameraTiltSpeed = 0;
+double cameraPan = 0;
+double cameraTilt = 0;
+double cameraPanSpeed = 0;
+double cameraTiltSpeed = 0;
 
-  void publishCamera();
+ros::Publisher flipperCmdAbsPub_;
+ros::Publisher flipperCmdRelPub_;
 
-  void joyCallback(const sensor_msgs::JoyConstPtr joystick) {
+double flipperFrontCommand;
+double lastFlipperFrontCommand;
+bool flipper_front_stop;
+bool flipper_front_up;
+bool flipper_front_down;
+double currentStateFlipperFront;
+
+int button_flipper_front_down;
+int button_flipper_front_up;
+
+int axis_flipper_front_up_down;
+int axis_flipper_front_left_right;
+
+double flipper_sensitivity;
+double flipper_front_position_absolute_up;
+double flipper_front_position_absolute_down;
+double flipper_front_position_absolute_center;
+
+
+void publishCamera();
+
+void publishFlipperFrontAbsolute(double position){
+    std_msgs::Float64 msg;
+    msg.data = position;
+    flipperCmdAbsPub_.publish(msg);
+}
+
+void publishFlipperFrontRelative(double movement){
+    std_msgs::Float64 msg;
+    msg.data = movement;
+    flipperCmdRelPub_.publish(msg);
+}
+
+void joyCallback(const sensor_msgs::JoyConstPtr joystick) {
     if (axis_speed > 0 && (size_t) axis_speed <= joystick->axes.size()) {
-      float value = joystick->axes[axis_speed-1];
-      if (value >= 0.0)
-        motionCommand.linear.x = value * speedForward;
-      else
-        motionCommand.linear.x = value * speedBackward;
+        float value = joystick->axes[axis_speed-1];
+        if (value >= 0.0)
+            motionCommand.linear.x = value * speedForward;
+        else
+            motionCommand.linear.x = value * speedBackward;
 
-      if (button_slow > 0 && (size_t) button_slow <= joystick->buttons.size() && joystick->buttons[button_slow-1]) {
-        motionCommand.linear.x *= slowFactor;
-      }
+        if (button_slow > 0 && (size_t) button_slow <= joystick->buttons.size() && joystick->buttons[button_slow-1]) {
+            motionCommand.linear.x *= slowFactor;
+        }
     }
 
     //Brake not applicable on many vehicles
@@ -85,12 +120,12 @@ namespace JoyTeleop {
     //}
 
     if (axis_steer > 0 && (size_t) axis_steer <= joystick->axes.size()) {
-      motionCommand.angular.z = joystick->axes[axis_steer-1] * steerAngle * M_PI/180.0;
+        motionCommand.angular.z = joystick->axes[axis_steer-1] * steerAngle * M_PI/180.0;
 
-      //Angle not applicable on many vehicles
-      //motionCommand.steerAngleRear  = motionCommand.steerAngleFront;
+        //Angle not applicable on many vehicles
+        //motionCommand.steerAngleRear  = motionCommand.steerAngleFront;
 
-      /*
+        /*
       if (button_crab > 0 && (size_t) button_crab <= joystick->buttons.size()) {
         if (joystick->buttons[button_crab-1]) {
           if (motionCommand.speed >= 0.0) {
@@ -103,22 +138,65 @@ namespace JoyTeleop {
       */
     }
 
+    bool current_flipper_up = false;
+    bool current_flipper_down = false;
+    if (button_flipper_front_down > 0 && (size_t) button_flipper_front_down <= joystick->buttons.size()){
+        current_flipper_up = joystick->buttons[button_flipper_front_down-1];
+    }
+
+
+    if (button_flipper_front_up > 0 && (size_t) button_flipper_front_up <= joystick->buttons.size())
+    {
+        current_flipper_down = joystick->buttons[button_flipper_front_up-1];
+    }
+
+    if((flipper_front_down && !current_flipper_down) || (flipper_front_up && !current_flipper_up)){
+        flipper_front_stop = true;
+    }
+    flipper_front_down = current_flipper_down;
+    flipper_front_up = current_flipper_up;
+
+
+    if (axis_flipper_front_up_down > 0 && (size_t) axis_flipper_front_up_down <= joystick->axes.size()) {
+        if(fabs(joystick->axes[axis_flipper_front_up_down-1]) > 0.1){
+            if(joystick->axes[axis_flipper_front_up_down-1] > 0){
+                publishFlipperFrontAbsolute(flipper_front_position_absolute_up);
+            }else{
+                publishFlipperFrontAbsolute(flipper_front_position_absolute_down);
+            }
+        }
+    }
+
+    if (axis_flipper_front_left_right > 0 && (size_t) axis_flipper_front_left_right <= joystick->axes.size()) {
+        if(fabs(joystick->axes[axis_flipper_front_left_right-1]) > 0.1){
+            publishFlipperFrontAbsolute(flipper_front_position_absolute_center);
+        }
+    }
+
+    if (axis_cameraPan > 0 && (size_t) axis_cameraPan <= joystick->axes.size()) {
+        cameraPanSpeed = joystick->axes[axis_cameraPan-1] * cameraSpeed * M_PI/180.0;
+    }
+
     motionCommandOutput.publish(motionCommand);
 
     if (axis_cameraPan > 0 && (size_t) axis_cameraPan <= joystick->axes.size()) {
-      cameraPanSpeed = joystick->axes[axis_cameraPan-1] * cameraSpeed * M_PI/180.0;
+        cameraPanSpeed = joystick->axes[axis_cameraPan-1] * cameraSpeed * M_PI/180.0;
     }
     if (axis_cameraTilt > 0 && (size_t) axis_cameraTilt <= joystick->axes.size()) {
-      cameraTiltSpeed = joystick->axes[axis_cameraTilt-1] * cameraSpeed * M_PI/180.0;
+        cameraTiltSpeed = joystick->axes[axis_cameraTilt-1] * cameraSpeed * M_PI/180.0;
     }
     if (button_cameraReset > 0 && (size_t) button_cameraReset <= joystick->buttons.size() && joystick->buttons[button_cameraReset-1]) {
-      cameraPan = 0;
-      cameraTilt = 0;
-      publishCamera();
+        cameraPan = 0;
+        cameraTilt = 0;
+        publishCamera();
     }
-  }
+}
 
-  void moveCamera(double dt) {
+void flipperFrontCallback(const std_msgs::Float64ConstPtr state_front_flipper) {
+    currentStateFlipperFront = state_front_flipper->data;
+}
+
+void moveCamera(double dt) {
     if (cameraPanSpeed == 0.0 && cameraTiltSpeed == 0.0) return;
 
     cameraPan += dt * cameraPanSpeed;
@@ -130,9 +208,9 @@ namespace JoyTeleop {
     if (cameraTilt < -cameraMaxTiltUp  *M_PI/180.0) cameraTilt = -cameraMaxTiltUp  *M_PI/180.0;
 
     publishCamera();
-  }
+}
 
-  void publishCamera() {
+void publishCamera() {
     cameraCommand.header.stamp = ros::Time::now();
     cameraCommand.header.frame_id = cameraMode;
     cameraCommand.quaternion.w =  cos(cameraPan/2)*cos(cameraTilt/2);
@@ -141,47 +219,72 @@ namespace JoyTeleop {
     cameraCommand.quaternion.z =  sin(cameraPan/2)*cos(cameraTilt/2);
 
     cameraCommandOutput.publish(cameraCommand);
-  }
+}
 }; // namespace JoyTeleop
 
 using namespace JoyTeleop;
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, ROS_PACKAGE_NAME);
-  ros::NodeHandle n;
+    ros::init(argc, argv, ROS_PACKAGE_NAME);
+    ros::NodeHandle n;
 
-  joyInput = n.subscribe<sensor_msgs::Joy>("joy", 10, joyCallback);
-  motionCommandOutput = n.advertise<geometry_msgs::Twist>("cmd_vel", 10, false);
-  cameraCommandOutput = n.advertise<geometry_msgs::QuaternionStamped>("camera/command", 10, false);
+    joyInput = n.subscribe<sensor_msgs::Joy>("joy", 10, joyCallback);
+    motionCommandOutput = n.advertise<geometry_msgs::Twist>("cmd_vel", 10, false);
+    cameraCommandOutput = n.advertise<geometry_msgs::QuaternionStamped>("camera/command", 10, false);
 
-  ros::param::param("~axis_speed", JoyTeleop::axis_speed, 0);
-  ros::param::param("~speed_forward", JoyTeleop::speedForward, 1.0);
-  ros::param::param("~speed_backward", JoyTeleop::speedBackward, 1.0);
-  ros::param::param("~axis_steer", JoyTeleop::axis_steer, 0);
-  ros::param::param("~steer_angle", JoyTeleop::steerAngle, 30.0);
-  ros::param::param("~button_brake", JoyTeleop::button_brake, 0);
-  ros::param::param("~button_crab", JoyTeleop::button_crab, 0);
-  ros::param::param("~button_slow", JoyTeleop::button_slow, 0);
-  ros::param::param("~slow_factor", JoyTeleop::slowFactor, 0.5);
+    flipperCmdAbsPub_ = n.advertise<std_msgs::Float64>("/flipper_control/command/absolute", 10, false);
+    flipperCmdRelPub_ = n.advertise<std_msgs::Float64>("/flipper_control/command/relative", 10, false);
 
-  ros::param::param("~axis_camera_pan", JoyTeleop::axis_cameraPan, 0);
-  ros::param::param("~axis_camera_tilt", JoyTeleop::axis_cameraTilt, 0);
-  ros::param::param("~button_camera_reset", JoyTeleop::button_cameraReset, 0);
-  ros::param::param("~camera_mode", JoyTeleop::cameraMode, std::string("base_stabilized"));
-  ros::param::param("~camera_speed", JoyTeleop::cameraSpeed, 60.0);
-  ros::param::param("~camera_max_pan", JoyTeleop::cameraMaxPan, 120.0);
-  ros::param::param("~camera_max_tilt_down", JoyTeleop::cameraMaxTiltDown, 30.0);
-  ros::param::param("~camera_max_tilt_up", JoyTeleop::cameraMaxTiltUp, 30.0);
+    ros::param::param("~axis_speed", JoyTeleop::axis_speed, 0);
+    ros::param::param("~speed_forward", JoyTeleop::speedForward, 1.0);
+    ros::param::param("~speed_backward", JoyTeleop::speedBackward, 1.0);
+    ros::param::param("~axis_steer", JoyTeleop::axis_steer, 0);
+    ros::param::param("~steer_angle", JoyTeleop::steerAngle, 30.0);
+    ros::param::param("~button_brake", JoyTeleop::button_brake, 0);
+    ros::param::param("~button_crab", JoyTeleop::button_crab, 0);
+    ros::param::param("~button_slow", JoyTeleop::button_slow, 0);
+    ros::param::param("~slow_factor", JoyTeleop::slowFactor, 0.5);
 
-  ros::Rate rate(25.0);
-  while(ros::ok()) {
-    if (motionCommand.linear.x != 0.0 ||
-        motionCommand.angular.z != 0.0)
-      motionCommandOutput.publish(motionCommand);
-    moveCamera(rate.expectedCycleTime().toSec());
-    ros::spinOnce();
-    rate.sleep();
-  }
+    ros::param::param("~axis_camera_pan", JoyTeleop::axis_cameraPan, 0);
+    ros::param::param("~axis_camera_tilt", JoyTeleop::axis_cameraTilt, 0);
+    ros::param::param("~button_camera_reset", JoyTeleop::button_cameraReset, 0);
+    ros::param::param("~camera_mode", JoyTeleop::cameraMode, std::string("base_stabilized"));
+    ros::param::param("~camera_speed", JoyTeleop::cameraSpeed, 60.0);
+    ros::param::param("~camera_max_pan", JoyTeleop::cameraMaxPan, 120.0);
+    ros::param::param("~camera_max_tilt_down", JoyTeleop::cameraMaxTiltDown, 30.0);
+    ros::param::param("~camera_max_tilt_up", JoyTeleop::cameraMaxTiltUp, 30.0);
 
-  return 0;
+    ros::param::param("~button_flipper_front_down", JoyTeleop::button_flipper_front_down, 5);
+    ros::param::param("~button_flipper_front_up", JoyTeleop::button_flipper_front_up, 7);
+    ros::param::param("~axis_flipper_front_up_down", JoyTeleop::axis_flipper_front_up_down, 6);
+    ros::param::param("~axis_flipper_front_left_right", JoyTeleop::axis_flipper_front_left_right, 5);
+    ros::param::param("~flipper_sensitivity", JoyTeleop::flipper_sensitivity, 0.2);
+    ros::param::param("~flipper_front_position_absolute_up", JoyTeleop::flipper_front_position_absolute_up, 1.0);
+    ros::param::param("~flipper_front_position_absolute_down", JoyTeleop::flipper_front_position_absolute_down, -1.0);
+    ros::param::param("~flipper_front_position_absolute_center", JoyTeleop::flipper_front_position_absolute_center, 0.0);
+
+    flipper_front_down = false;
+    flipper_front_up = false;
+
+    ros::Rate rate(25.0);
+    while(ros::ok()) {
+        if (motionCommand.linear.x != 0.0 ||
+                motionCommand.angular.z != 0.0)
+            motionCommandOutput.publish(motionCommand);
+        moveCamera(rate.expectedCycleTime().toSec());
+
+        if(flipper_front_stop){
+          publishFlipperFrontRelative(0);
+          flipper_front_stop = false;
+        } else if(flipper_front_up){
+            publishFlipperFrontRelative(1.0*flipper_sensitivity);
+        }else if(flipper_front_down){
+            publishFlipperFrontRelative(-1.0*flipper_sensitivity);
+        }
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
 }
